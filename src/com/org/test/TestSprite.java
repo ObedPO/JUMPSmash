d4e4147a698d614b1f8e86a39911546a5087d4eb
Kizuree <aldenis1231@hotmail.com>
2019-06-13 08:39:17 -0400

    Uupdated Code

--- src/com/org/test/TestSprite.java
+++ src/com/org/test/TestSprite.java
@@ -1,4 +1,37 @@
 package com.org.test;
 
-public class TestSprite {
+import com.game.Game;
+import com.graphics.Renderer;
+import org.input.Input;
+import org.object.Sprite;
+
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+public class TestSprite extends Sprite {
+
+    public TestSprite(float posX, float posY){
+        super(posX, posY, "");
+
+        try {
+            image = Renderer.loadImage("/com/resources/images/goku.png");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void update(float deltaTime){
+        if(Input.getKey(KeyEvent.VK_W)){
+            posY -= 100*deltaTime;
+        }
+
+        if(Input.getKey(KeyEvent.VK_S)){
+            posY += 100*deltaTime;
+        }
+
+
+    }
+
+
 }
